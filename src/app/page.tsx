1845847e2659e764feadf182072007644f2df343
zolboo-gtn <72186937+zolboo-gtn@users.noreply.github.com>
2023-10-27 13:03:07 +0800

    miscs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,16 @@ import { use } from "react";
 
 import prisma from "@/db";
 
+export const dynamic = "force-dynamic";
 const HomePage: React.FC = () => {
   const users = use(prisma.user.findMany({ cacheStrategy: { ttl: 60 } }));
   console.log("HomePage", { users });
 
-  return <main>{`HOME PAGE`}</main>;
+  return (
+    <main>
+      {`HOME PAGE`}
+      <div>{JSON.stringify(users)}</div>
+    </main>
+  );
 };
 export default HomePage;
