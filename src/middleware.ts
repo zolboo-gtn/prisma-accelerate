d2501caf01a6b3427cea0fc8af00f399631939c8
zolboo-gtn <72186937+zolboo-gtn@users.noreply.github.com>
2023-10-27 14:38:38 +0800

    miscs

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,10 @@
 import type { NextMiddleware } from "next/server";
 
-import prisma from "@/db";
+// import prisma from "@/db";
 
 export const config = {
   matcher: ["/((?!api|_next|assets|favicon.ico).*)"],
 };
 export const middleware: NextMiddleware = async (request) => {
-  const users = await prisma.user.findMany({ cacheStrategy: { ttl: 60 } });
+  // const users = await prisma.user.findMany({ cacheStrategy: { ttl: 60 } });
 };
