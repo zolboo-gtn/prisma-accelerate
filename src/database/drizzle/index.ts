6cf4f4aebfcd8381f9e7d9486a7e57a7e7f8071e
zolboo-gtn <72186937+zolboo-gtn@users.noreply.github.com>
2023-10-27 15:50:48 +0800

    miscs

--- src/database/drizzle/index.ts
+++ src/database/drizzle/index.ts
@@ -1,4 +1,3 @@
-// import { drizzle } from "drizzle-orm/neon-serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon, neonConfig } from "@neondatabase/serverless";
 
@@ -12,8 +11,5 @@ export const schema = {
 
 neonConfig.fetchConnectionCache = true;
 
-// const client = new Pool({
-//   connectionString: process.env.DRIZZLE_DATABASE_URL!,
-// });
 const client = neon(process.env.DRIZZLE_DATABASE_URL!);
 export const db = drizzle(client, { schema });
